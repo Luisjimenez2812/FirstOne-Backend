38d601dae18bc587b6da110a41829493c22426ee
Rous23 <rosaavila14.2015@gmail.com>
2021-12-06 14:55:39 -0600

    Archivos de estructura e información para la base de datos

---
+++ data/estructura.js
@@ -0,0 +1,6 @@
+use firstOne;
+
+db.createCollection('clientes');
+db.createCollection('empresas');
+db.createCollection('motoristas');
+db.createCollection('ordenesDisponibles');
---
+++ data/informacion.js
@@ -0,0 +1,159 @@
+db.clientes.insertMany([
+    {
+        "nombres": "",
+        "apellidos": "",
+        "usuario": "",
+        "genero": "",
+        "correo": "",
+        "telefono": "",
+        "fechaNacimiento": {
+            "dia": "",
+            "mes": "",
+            "anio": ""
+        },
+        "contrasena": "",
+        "carrito": {
+            "productos": [
+                {
+                    "nombre": "",
+                    "descripcion": "",
+                    "precio": "",
+                    "imgUrl": "",
+                    "cantidad": ""
+                },
+                {
+                    "nombre": "",
+                    "descripcion": "",
+                    "precio": "",
+                    "imgUrl": "",
+                    "cantidad": ""
+                },
+            ],
+            "total": ""
+        },
+        "historialOrdenes": [
+            {
+                "estado": "",
+                "nombre": ""
+            }
+        ],
+        "ordenesPendientesEntrega": [
+            {
+                "estado": "",
+                "nombre": ""
+            }
+        ],
+        "empresasFavoritas": [
+            {
+                "nombre": "",
+                "imgUrl": ""
+            }
+        ],
+        "busquedas": [
+            {
+                "contenido": ""
+            },
+            {
+                "contenido": ""
+            }
+        ],
+        "direccionesEntrega": [
+            {
+                "direccion": ""
+            }
+        ]
+    },
+    {
+        "nombres": "",
+        "apellidos": "",
+        "usuario": "",
+        "genero": "",
+        "correo": "",
+        "telefono": "",
+        "fechaNacimiento": {
+            "dia": "",
+            "mes": "",
+            "anio": ""
+        },
+        "contrasena": "",
+        "carrito": {
+            "productos": [
+                {
+                    "nombre": "",
+                    "descripcion": "",
+                    "precio": "",
+                    "imgUrl": "",
+                    "cantidad": ""
+                },
+                {
+                    "nombre": "",
+                    "descripcion": "",
+                    "precio": "",
+                    "imgUrl": "",
+                    "cantidad": ""
+                },
+            ],
+            "total": ""
+        },
+        "historialOrdenes": [
+            {
+                "estado": "",
+                "nombre": ""
+            }
+        ],
+        "ordenesPendientesEntrega": [
+            {
+                "estado": "",
+                "nombre": ""
+            }
+        ],
+        "empresasFavoritas": [
+            {
+                "nombre": "",
+                "imgUrl": ""
+            }
+        ],
+        "busquedas": [
+            {
+                "contenido": ""
+            },
+            {
+                "contenido": ""
+            }
+        ],
+        "direccionesEntrega": [
+            {
+                "direccion": ""
+            }
+        ]
+    }
+])
+
+db.motoristas.insertMany([
+    {
+        "nombres": "",
+        "apellidos": "",
+        "usuario": "",
+        "genero": "",
+        "correo": "",
+        "telefono": "",
+        "fechaNacimiento": {
+            "dia": "",
+            "mes": "",
+            "anio": ""
+        },
+        "contrasena": "",
+        "historialEntregas": [
+            {
+                "estado": "",
+                "titulo": ""
+            }
+        ],
+        "ordenesTomadas": [
+            {
+                "estado": "",
+                "titulo": ""
+            }
+        ]
+    }
+])
