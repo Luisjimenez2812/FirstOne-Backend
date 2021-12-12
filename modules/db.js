45c19eeaaa3a03b65637967656a3dddb6956f91a
Rous23 <rosaavila14.2015@gmail.com>
2021-12-11 23:00:31 -0600

    configurando index.js

--- modules/db.js
+++ modules/db.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
-const db = 'firstone';
-const port = '27017';
-const host = 'localhost';
+const usuario = 'lromeroj';
+const contrasena = 'Lerj.2812';
+const bd = 'firstone';
 
-const MONGODB_URI = `mongodb+srv://lromeroj:Lerj.2812@cluster0.ewltm.mongodb.net/firstone?retryWrites=true&w=majority`
+const MONGODB_URI = `mongodb+srv://${usuario}:${contrasena}@cluster0.ewltm.mongodb.net/${bd}?retryWrites=true&w=majority`
 
 
 class Database{
